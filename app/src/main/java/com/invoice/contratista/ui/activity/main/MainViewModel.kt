f8895017fb0671be8ad1ec171ab55455fca7b20d
Alejandra Jimenez Avalos <55304400+alejandraJA@users.noreply.github.com>
2022-11-17 17:38:31 -0600

    Update dependencies

--- app/src/main/java/com/invoice/contratista/ui/activity/main/MainViewModel.kt
+++ app/src/main/java/com/invoice/contratista/ui/activity/main/MainViewModel.kt
@@ -1,8 +1,13 @@
 package com.invoice.contratista.ui.activity.main
 
 import androidx.lifecycle.ViewModel
+import com.invoice.contratista.data.shared_preferences.UtilsManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel()
+class MainViewModel @Inject constructor(utilsManager: UtilsManager) : ViewModel() {
+    init {
+        utilsManager.getIdPart()
+    }
+}
